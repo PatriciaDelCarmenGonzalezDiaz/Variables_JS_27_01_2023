d906aff88754fc98e156355e92dcabe30e9f5a2f
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-01-27 22:05:23 -0500

    uso de variables

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let apellido = "Gonzalez";
+let peso = 60.8;
+let mujer = true;
+let identificacion = {
+    tipoDocto: "Cédula",
+    noDocto: 325987,
+    fechaNac: "1968-01-20"
+};
+
+console.log('El apellido es: ' + apellido);
+console.log('El peso es: ' + peso);
+console.log('Es mujer?: ' + mujer);
+console.log('Identificacion: ' + identificacion.tipoDocto + ': ' + identificacion.noDocto + ", Fecha Nacimiento: " + identificacion.fechaNac);
+
